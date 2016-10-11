a3fe99dafa0f7f257bea6a384d54719692406817
jesse <jesseaborden@gmail.com>
2016-10-11 14:16:57 -0700

    more changes

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const marvelRouter = require('./router').marvelRouter;
 require('./database');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
